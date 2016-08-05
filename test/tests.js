c8bc6c2fb48576b174ea34d6a0974690f34cc676
Andreas Hubert <andreas.hubert@gmx.at>
2016-08-05 10:23:53 +0200

    * corrected some typos

--- test/tests.js
+++ test/tests.js
@@ -5,6 +5,7 @@ describe("blabla", function() {
   it("should do something", function(done) {
     var ps = new Ps();
     ps.get(function(err, data) {
+      console.log("data: ", data);
       try {
         assert(data instanceof Array);
         assert(data.length > 0);
