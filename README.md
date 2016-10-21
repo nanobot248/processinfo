222b9a93a66862919ba9a5486f3d33f0d6b86bd2
Andreas Hubert <andreas.hubert@gmx.at>
2016-10-22 00:52:15 +0200

    * added support for selecting which columns to return  * updated README.md documentation  * increased version to 0.4

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # processinfo
-nodejs module for getting process information. Uses "ps" and currently only works in Linux and other environments with a compatible "ps" command.
+nodejs module for getting process information. Uses "ps" and currently only
+works on Linux systems and other environments with a compatible "ps" command.
 
 # Usage
+## Get all columns
+This snippet lists all columns known to the ProcessList class by using the
+column names listed in the `ProcessList.defaultColumns` array.
+
 ```javascript
 var Ps = require("../lib/package.js");
 
@@ -11,3 +16,30 @@ ps.get(function(err, data) {
 });
 ```
 
+## Only get some specific columns
+This code returns only four columns.
+```javascript
+var Ps = require("../lib/package.js");
+
+var ps = new Ps({
+  columns: ["processId", "parentProcessId", "lightweightProcessId", "command"]
+});
+ps.get(function(err, data) {
+  console.log("Process information: ", data);
+});
+```
+
+## Don't list threads
+The following code only lists real processes, but skips all lightweight
+processes (threads).
+
+```javascript
+var Ps = require("../lib/package.js");
+
+var ps = new Ps({
+  listThreads: false
+});
+ps.get(function(err, data) {
+  console.log("Process information: ", data);
+});
+```
--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,21 @@
-var assert = require("assert");
-var Ps = require("../lib/package.js");
+const assert = require("assert");
+const Ps = require("../lib/package.js");
+const _ = require("lodash");
 
-describe("blabla", function() {
-  it("should do something", function(done) {
+describe("get all available columns and list threads", function() {
+  it("returned columns should be equal to ProcessList.defaultColumns", function(done) {
     var ps = new Ps();
     ps.get(function(err, data) {
-      console.log("data: ", data);
       try {
         assert(data instanceof Array);
         assert(data.length > 0);
+        var columns = Ps.defaultColumns;
+        var proc = data[0];
+        var keys = Object.keys(proc);
+        var diff1 = _.difference(columns, keys);
+        var diff2 = _.difference(keys, columns);
+        assert(diff1.length < 1);
+        assert(diff2.length < 1);
         done();
       } catch(ex) {
         done(ex);
@@ -17,3 +24,58 @@ describe("blabla", function() {
   });
 });
 
+describe("get only some columns and list threads", function() {
+  it("returned columns should be [processId, parentProcessId, command]", function(done) {
+    var columns = ["processId", "parentProcessId", "command"];
+    var ps = new Ps({
+      columns: columns
+    });
+    ps.get(function(err, data) {
+      try {
+        assert(data instanceof Array);
+        assert(data.length > 0);
+        var proc = data[0];
+        var keys = Object.keys(proc);
+        var diff1 = _.difference(columns, keys);
+        var diff2 = _.difference(keys, columns);
+        assert(diff1.length < 1);
+        assert(diff2.length < 1);
+        done();
+      } catch(ex) {
+        done(ex);
+      }
+    });
+  });
+});
+
+describe("get only real process, no threads", function() {
+  it("Number of distinct process IDs should be the same as the number of distinct thread IDs", function(done) {
+    var columns = ["processId", "lightweightProcessId"];
+    var ps = new Ps({
+      columns: columns,
+      listThreads: false
+    });
+    ps.get(function(err, data) {
+      try {
+        assert(data instanceof Array);
+        assert(data.length > 0);
+        var proc = data[0];
+        var keys = Object.keys(proc);
+        var diff1 = _.difference(columns, keys);
+        var diff2 = _.difference(keys, columns);
+        assert(diff1.length < 1);
+        assert(diff2.length < 1);
+        var processIds = _.map(data, function(proc) {
+          return proc.processId
+        });
+        var threadIds = _.map(data, function(proc) {
+          return proc.lightweightProcessId
+        });
+        assert(_.uniq(processIds).length == _.uniq(threadIds).length);
+        done();
+      } catch(ex) {
+        done(ex);
+      }
+    });
+  });
+});
